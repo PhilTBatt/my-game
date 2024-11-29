cb307a78a6349232e1f3880ff134b6127ae2ce84
PhilTBatt <philtbatt@gmail.com>
2024-11-29 17:41:24 +0000

    panels on battle screen

--- src/phaser/BattleScreen.ts
+++ src/phaser/BattleScreen.ts
@@ -15,9 +15,18 @@ class BattleScreen extends Phaser.Scene {
         this.add.rectangle(500, 300, 1000, 600, 0x00ffff)
         this.add.rectangle(500, 375, 1000, 450, 0x40CF55)
         this.add.rectangle(500, 450, 1000, 300, 0x929292)
+
+        this.buttonPanel = new DefaultButtonPanel(this)
+        this.add.existing(this.buttonPanel)
     }
 
     update() {
+        if (this.buttonPanel?.showAttacks) {
+            // const attackPanel = new AttackPanel(this)
+            this.buttonPanel.visible = false
+            // this.add.existing(attackPanel)
+            
+        }
     }
 }
 
--- src/phaser/game-objects/button-panels/DefaultButtonPanel.ts
+++ src/phaser/game-objects/button-panels/DefaultButtonPanel.ts
@@ -11,11 +11,15 @@ export default class DefaultButtonPanel extends Phaser.GameObjects.Container {
     showStats: boolean = false
     
     constructor(scene: Phaser.Scene) {
-        super(scene, 0, 0)
+        super(scene, 200, 365)
         
-        this.attackButton = new Button(scene, 66.67, 20, 400, 120, 'Attack', 0xE6E6E6, 0xF80000, '50px', () => this.showAttacks = true)
+        this.attackButton = new Button(scene, 66.67, 20, 400, 120, 'Attack', 0xE6E6E6, 0xF80000, '50px',  () => this.showAttacks = true)
+        this.add(this.attackButton)
         this.defendButton = new Button(scene, 533.33, 20, 400, 120, 'Defend', 0xE6E6E6, 0x00FF00, '50px', () => this.showDefends = true)
+        this.add(this.defendButton)
         this.itemsButton = new Button(scene, 66.67, 160, 400, 120, 'Items', 0xE6E6E6, 0x0000FF, '50px', () => this.showItems = true)
+        this.add(this.itemsButton)
         this.statsButton = new Button(scene, 533.33, 160, 400, 120, 'Stats', 0xE6E6E6, 0xFFFF00, '50px', () => this.showStats = true)
+        this.add(this.statsButton)
     }
 }

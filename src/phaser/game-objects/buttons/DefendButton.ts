83c1f241228f27aa1c3a2acb28afa6fdbdaac337
PhilTBatt <philtbatt@gmail.com>
2024-12-02 13:58:00 +0000

    better defend buttons

--- src/phaser/game-objects/buttons/DefendButton.ts
+++ src/phaser/game-objects/buttons/DefendButton.ts
@@ -3,11 +3,49 @@ import { Action } from "../../types";
 import Button from "./Button";
 
 export default class DefendButton extends Button {
+    blockIcon: Phaser.GameObjects.Image
+    staminaIcon: Phaser.GameObjects.Image
+
     constructor(scene: BattleScreen, x: number, y: number, defend: Action) {
-        super(scene, x, y, 440, 103, `${defend.name}: ${defend.value}`, 0xE6E6E6, 0x003EF8, 10, '70px',
+        super(scene, x, y, 440, 103, ``, 0xE6E6E6, 0x003EF8, 10, '70px',
             () => scene.time.delayedCall(200, () => {
                 scene.player?.block(defend.value)
                 scene.player?.changeStamina(-defend.stamina)
             }))
+
+        const nameText = scene.add.text(-110, 0, `${defend.name}:`, {fontSize: '60px', color: '#000000', fontFamily: 'Arial'})
+        nameText.setOrigin(0.5, 0.5)
+        
+        const valueText = scene.add.text(nameText.width / 2 - 70, 0, `${defend.value}`, {fontSize: '60px', color: '#000000', fontFamily: 'Arial'})
+        valueText.setOrigin(0.5)
+        this.blockIcon = scene.add.image(nameText.width / 2 - 20, 0, 'damage-icon').setScale(0.25)
+
+
+        const staminaText = scene.add.text(nameText.width / 2 + 40, 0, `${defend.stamina}`, {fontSize: '60px', color: '#000000', fontFamily: 'Arial'})
+        staminaText.setOrigin(0.5)
+        this.staminaIcon = scene.add.image(nameText.width / 2 + 90, 0, 'stamina-icon').setScale(0.25)
+
+
+        this.add(nameText)
+        this.add(this.blockIcon)
+        this.add(valueText)
+        this.add(this.staminaIcon)
+        this.add(staminaText)
+
+        this.on('pointerover', () => {
+            nameText.setScale(1.1)
+            valueText.setScale(1.15)
+            staminaText.setScale(1.15)
+            this.blockIcon.setScale(0.3)
+            this.staminaIcon.setScale(0.3)
+          })
+            
+          this.on('pointerout', () => {
+            nameText.setScale(1)
+            valueText.setScale(1)
+            staminaText.setScale(1)
+            this.blockIcon.setScale(0.25)
+            this.staminaIcon.setScale(0.25)
+          })
     }
 }
